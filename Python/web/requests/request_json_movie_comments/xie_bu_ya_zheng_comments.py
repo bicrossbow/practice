1961739ee7cbbe0e33710bfb4a1ed8ab9e5dae60
Nu Zhang <crossbow.zn@gmail.com>
2018-07-23 23:46:29 -0700

    add request/url example and Geo pyecharts example

---
+++ Python/web/requests/request_json_movie_comments/xie_bu_ya_zheng_comments.py
@@ -0,0 +1,66 @@
+"""
+This is to study request and url to get data from online json 'database' and then use Geo to plot data visualization
+
+https://www.jiqizhixin.com/articles/2018-07-22
+"""
+
+
+#coding=utf-8
+import requests
+import json
+import time
+import random
+
+def get_one_page(url):
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
+    }
+    response = requests.get(url,headers=headers)
+    print(type(response))
+    print(response)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+def parse_one_page(html):
+    content = json.loads(html)
+    data = content['cmts']
+    for item in data:
+        yield{
+            'comment':item['content'],
+            'date':item['time'].split(' ')[0],
+            'rate':item['score'],
+            'city':item['cityName'],
+            'nickname':item['nickName']
+        }
+
+def save_to_text(file_name):
+    for i in range(1,1001):
+        url = 'http://m.maoyan.com/mmdb/comments/movie/248566.json?_v_=yes&offset=' + str(i)
+        html = get_one_page(url)
+        print('Saving page{}'.format(i))
+        for item in parse_one_page(html):
+            with open(file_name,'a',encoding='utf-8') as f:
+                f.write('{date},{nickname},{city},{rate},{comment}\n'.format(date=item['date'],nickname=item['nickname'],city=item['city'],rate=item['rate'],comment=item['comment'].replace('\n',' ')))
+                #f.write(item['date'] + ',' + item['nickname'] + ',' + item['city'] + ',' +str(item['rate'])+','+item['comment']+'\n')
+        time.sleep(5 + float(random.randint(1, 100)) / 20)
+
+def remove_dupline(infile,outfile):
+    infopen = open(infile,'r',encoding='utf-8')
+    outopen = open(outfile,'w',encoding='utf-8')
+    lines = infopen.readlines()
+    list_line = []
+
+    for line in lines:
+        if line not in list_line:
+            list_line.append(line)
+            outopen.write(line)
+    infopen.close()
+    outopen.close()
+
+if __name__ == '__main__':
+    file_name = 'xie_bu_ya_zheng_comments.txt'
+    file_name_no_dup = 'xie_bu_ya_zheng_comments_remove_dup.txt'
+    save_to_text(file_name)
+    remove_dupline(file_name,outffile_name_no_dupile)
+    
---
+++ Python/web/requests/request_json_movie_comments/xie_bu_ya_zheng_comments_geo.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+
+from pyecharts import Style
+from pyecharts import Geo
+
+import os
+
+
+def get_dic_city_name_count(list_city):
+    dic_city_name_count = {}
+    for city in set(list_city):
+        dic_city_name_count[city] = list_city.count(city)
+    return dic_city_name_count
+
+if __name__ == '__main__':
+
+    file_name_comments_org = 'xie_bu_ya_zheng_comments.txt'
+    file_name_comments = 'xie_bu_ya_zheng_comments_backup.txt'
+    os.system('cp {scr} {des}'.format(scr=file_name_comments_org,des=file_name_comments))
+    
+    # load city info
+    city = []
+    with open(file_name_comments,mode='r',encoding='utf-8') as f:
+        rows = f.readlines()
+        for row in rows:
+            items_in_a_row = row.split(',')
+            if len(items_in_a_row) == 5 and len(items_in_a_row[2])>=1:
+                city.append(items_in_a_row[2].replace('\n',''))
+
+    city_num_comment_pair = []
+
+    style = Style(title_color = "#fff",title_pos = "center",width = 1200,height = 600,background_color = "#404a59")
+    geo = Geo("《邪不压正》粉丝人群地理位置","数据来源：恋习Python",**style.init_style)
+
+    dic_city_name_count = get_dic_city_name_count(city)
+    for city_name in dic_city_name_count:
+        coordinate=geo.get_coordinate(city_name)
+        if coordinate is not None:
+            city_num_comment_pair.append((city_name,dic_city_name_count[city_name]))
+            #style = Style(title_color = "#fff",title_pos = "center",width = 1200,height = 600,background_color = "#404a59")
+
+    attr,value= geo.cast(city_num_comment_pair)
+    # geo.add_coordinate("启东", 121.703, 31.87)
+    # geo.add_coordinate("新昌", 120.9039, 29.4998)
+    # geo.add_coordinate("璧山", 106.2273, 29.5920)
+   
+    geo.add("",attr,value,visual_range=[0, 50], maptype='china',visual_text_color="#fff",symbol_size=10,is_visualmap=True,is_piecewise=True,visual_split_number=4)
+    try:
+        geo.render("邪不压正-评论分布.html")
+    except ValueError as e:
+        print(e)
+   
